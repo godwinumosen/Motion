b2b153d821112452d882132bce36e339fb8a9688
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-23 02:14:02 -0700

    syntax error is been remove

--- motionlimited/settings.py
+++ motionlimited/settings.py
@@ -131,5 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-LOGIN_REDIRECT_URL = '/index/'
-LOGOUT_REDIRECT_URL = '/index/'
+LOGIN_REDIRECT_URL = '/home/'
+LOGOUT_REDIRECT_URL = '/home/'
