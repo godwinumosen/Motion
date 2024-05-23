9021fc0e2408414e838ee10c159781b1b14b8714
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-23 08:24:12 -0700

    Motion academy is onset

--- motion/views.py
+++ motion/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView,ListView
 from django.contrib.auth.decorators import login_required
 from .models import MotionMainPost
+from django.contrib import messages
 
 # Create your views here.
 
