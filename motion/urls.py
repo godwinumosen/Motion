ea668bf8cdad64ddb739e9c9bde2e2c99793e8de
Godwin Umosen <umosengodwin568@gmail.com>
2024-05-23 02:31:26 -0700

    syntax error is been remove

--- motion/urls.py
+++ motion/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import HomeView #ArticleDetailView,
 
 urlpatterns = [
-    path('', views.home, name='home.html'),
+    path('', HomeView.as_view(), name="home"),
+    path('home/', HomeView.as_view(), name='home'),
 ]
--- motion/views.py
+++ motion/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView,ListView
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def home (request):
-    return render (request, 'home.html',{})
+'''def home (request):
+    return render (request, 'home.html',{})'''
+
+def base (request):
+    return render(request,"base.html")
+
+#The main HomeView page
+class HomeView(ListView):
+    model = DeusMagnusMainPost
+    template_name = 'deus_magnus/home.html'
