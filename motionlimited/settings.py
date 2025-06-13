eefce55cb2da3695cd081b1cff4e10152830fa9c
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-13 13:49:23 +0100

    Out of Memory Debugging

--- motionlimited/settings.py
+++ motionlimited/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(cc!bc_8segafz%bwsi!8nbbdw*z0@@)q3y3dhz*%eqrug+key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
